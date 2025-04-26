f9185ab1bb7dfee7f26964ef594a65e3d7b170ba
petroniojs <petroniojs@yahoo.com.br>
2025-04-26 13:40:56 -0300

    Add files via upload

--- streamlit01.py
+++ streamlit01.py
@@ -24,7 +24,7 @@ import numpy as np
 import pandas as pd
 
 # MATPLOTLIB - fornece visualização de dados
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 # SEABORN - melhora a aparência dos gráficos do matplotlib
 import seaborn as sb
